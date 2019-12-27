ba7d6bc2e6fdd12978c28e27dea28fb9fad52ceb
takahumi0806 <yabuta0439@hotmail.com>
2019-12-27 19:21:54 +0900

    group-materials

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,7 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
   def index
+    
   end
 
   def new
@@ -19,11 +20,12 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    
   end
 
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(params[:id]), notice: 'グループを更新しました'
     else
       render :edit
     end
